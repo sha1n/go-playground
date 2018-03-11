dc7bc7f35105874854e4a99291efc1064cf5f12b
sha1n <sha1n@users.noreply.github.com>
2018-03-11 10:32:34 +0200

    basic hello http server

---
+++ http/hello_world_server_test.go
@@ -0,0 +1,28 @@
+package hello_http
+
+import "testing"
+import "net/http"
+import "io/ioutil"
+
+func check(msg string, err error, t *testing.T) {
+	if err != nil {
+		t.Errorf(msg, err)
+	}
+}
+
+func TestSimpleGetResponse(t *testing.T) {
+	StartHttpServer(":8080")
+
+	resp, err := http.Get("http://localhost:8080")
+	check("Request failed!", err, t)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	check("Failed to read from response", err, t)
+
+	bodyString := string(body)
+	t.Log("Got body:", bodyString)
+
+	StopHttpServer()
+}
