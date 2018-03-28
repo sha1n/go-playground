817ae25829d7207736301f6acb8beb32d6b7731f
sha1n <sha1n@users.noreply.github.com>
2018-03-28 17:13:22 +0300

    replaced relative module paths with standard go import paths

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"./ctrl"
-	"./datastruct"
-	"./json"
-	"./methods"
-	"./pkg"
-	"./routines"
+	"github.com/sha1n/go-playground/ctrl"
+	"github.com/sha1n/go-playground/datastruct"
+	"github.com/sha1n/go-playground/json"
+	"github.com/sha1n/go-playground/methods"
+	"github.com/sha1n/go-playground/pkg"
+	"github.com/sha1n/go-playground/routines"
 )
 
 func main() {
