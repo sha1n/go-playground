58a6f700432023194dedeab139a14249dea07424
Shai Nagar <shain@wix.com>
2018-03-28 14:44:33 +0300

    more elaborate routines example

--- routines/routines.go
+++ routines/routines.go
@@ -6,16 +6,46 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 )
 
-func executeInWorker(description string, waitGroup *sync.WaitGroup) {
+// Work - executable worker function definition
+type Work func()
+
+// WorkItem a stupid work item struct
+type WorkItem struct {
+	Description string
+	Executable  Work
+}
+
+// ExecuteInWorker - method for executing work items using go routines
+func (workItem *WorkItem) ExecuteInWorker(waitGroup *sync.WaitGroup) {
 	waitGroup.Add(1)
-	defer waitGroup.Done()
 
 	go func() {
-		fmt.Println("Executing", description)
+		fmt.Printf("\t[async] ~> Starting %s...\r\n", workItem.Description)
+		workItem.Executable()
+		fmt.Printf("\t[async] ~> %s done!\r\n", workItem.Description)
+
+		defer waitGroup.Done()
 	}()
 
+}
+
+func sleepyWorkerFactory(duration string) Work {
+	work := Work(func() {
+		d, _ := time.ParseDuration(duration)
+		time.Sleep(d)
+	})
+
+	return work
+}
+
+func sleepyWorkItemFactory(desc string, sleepDuration string) WorkItem {
+	exec := sleepyWorkerFactory(sleepDuration)
+	workItem := WorkItem{Description: desc, Executable: exec}
+
+	return workItem
 }
 
 // Run executing code in go routine..
@@ -30,7 +60,11 @@ func Run() {
 
 	waitGroup := &sync.WaitGroup{}
 
-	executeInWorker("Work Item", waitGroup)
+	workItem1 := sleepyWorkItemFactory("Work Item #1", "1s")
+	workItem1.ExecuteInWorker(waitGroup)
+
+	workItem2 := sleepyWorkItemFactory("Work Item #2", "0s")
+	workItem2.ExecuteInWorker(waitGroup)
 
 	fmt.Println("Press Ctrl+C to exit..")
 
