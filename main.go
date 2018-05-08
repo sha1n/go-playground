d599563ed305c56010cc8b59b779836e40138c82
sha1n <sha1n@users.noreply.github.com>
2018-05-08 18:34:28 +0300

    basic channels demo

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"github.com/sha1n/go-playground/cmd"
+	"github.com/sha1n/go-playground/chan"
+	"github.com/sha1n/go-playground/routines"
+	"github.com/sha1n/go-playground/refs"
 	"github.com/sha1n/go-playground/ctrl"
 	"github.com/sha1n/go-playground/datastruct"
-	"github.com/sha1n/go-playground/interf"
-	"github.com/sha1n/go-playground/json"
 	"github.com/sha1n/go-playground/methods"
+	"github.com/sha1n/go-playground/json"
 	"github.com/sha1n/go-playground/pkg"
-	"github.com/sha1n/go-playground/refs"
-	"github.com/sha1n/go-playground/routines"
+	"github.com/sha1n/go-playground/cmd"
+	"github.com/sha1n/go-playground/interf"
 )
 
 func main() {
@@ -21,5 +22,6 @@ func main() {
 	pkg.Demo()
 	cmd.Demo()
 	interf.Demo()
+	channels.Demo()
 	routines.Demo()
 }
